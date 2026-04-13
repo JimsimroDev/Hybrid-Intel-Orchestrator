5b9fb1e86463bbcb5597f42843294c24e5f05442
Jimmis Jhoan Simanca Rojas <122319039+JimsimroDev@users.noreply.github.com>
2026-04-12 22:50:02 -0500

    Restrict CORS to specific origin

--- src/main/java/com/jimsimrodev/orchestrator/config/CorsConfiguration.java
+++ src/main/java/com/jimsimrodev/orchestrator/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
+        registry.addMapping("/**").allowedOrigins("https://orchestrator.jimsimrodev.uk")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "CONNECT");
     }
 
